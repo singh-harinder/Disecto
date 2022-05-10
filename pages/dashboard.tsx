344a1a84fbe06e6e8c3be80a46b11868b1ed62c5
Harinder Singh <kinggouken611@gmail.com>
2022-05-10 12:13:10 +0530

    Fixed useEffect

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -7,24 +7,28 @@ function Dashboard() {
   const user = useUserQuery();
 
   useEffect(() => {
-    if (user.data?.authenticatedItem?.isAdmin === (false || undefined)) {
+    if (user.loading) {
+      return;
+    }
+
+    if (!user.data?.authenticatedItem?.isAdmin) {
       Router.push({
         pathname: "/",
       }).catch(() => {});
     }
-  });
+  }, [user.data?.authenticatedItem?.isAdmin, user.loading]);
 
   return (
     <div>
       <Head>
         <title>Disecto | Dashboard</title>
       </Head>
-      {user.data?.authenticatedItem?.isAdmin === (false || undefined) && (
+      {!user.data?.authenticatedItem?.isAdmin && (
         <div className="flex items-center justify-center text-4xl">
           You do not have permission to access this resource
         </div>
       )}
-      {user.data?.authenticatedItem?.isAdmin === true && (
+      {user.data?.authenticatedItem?.isAdmin && (
         <div className="flex items-center justify-center text-4xl">This is the admin dashboard</div>
       )}
     </div>
